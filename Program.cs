6595b6f1993ffabfe1a3ce36ce67e5fabc423a4c
student <student@itip.loc>
2019-09-14 11:11:12 +0300

    init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace bog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
